de13b3db8ada640bd48f86720be3593f937c2efd
AllifAmey <24247758+AllifAmey@users.noreply.github.com>
2022-10-22 23:58:36 +0100

    created build folder

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,6 +13,12 @@ const theme = createTheme({
     },
   },
 });
+/*
+https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60107f22e96be8bc2cc5785b_6002086f72b7277a6401e43e_sobremesa.jpeg - look at the white arrow at the bottom. Maybe add that.
+https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60107f2158f4bbbcec10c88f_6002086f72b727f54401e412_space-posters.jpeg - look at the bottom right , add that most likely
+
+https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60107f23208b459df8790224_6002086f72b7270da901e44a_personalized-cans.jpeg - maybe as inspiration ? Not sure. 
+*/
 function Hero() {
   return (
     <>
