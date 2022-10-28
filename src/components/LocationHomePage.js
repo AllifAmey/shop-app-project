d30257174409a5fe5d14426829d4f58474a89364
AllifAmey <24247758+AllifAmey@users.noreply.github.com>
2022-10-28 14:23:00 +0100

    prettified the locationhomepage

--- src/components/LocationHomePage.js
+++ src/components/LocationHomePage.js
@@ -1,11 +1,14 @@
 import React, { useState, useMemo } from "react";
 import styles from "./LocationHomePage.module.css";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import img from "../img/icons/openSign-icon.png";
+
 import Home from "./googleMap";
 
 function LocationHomePage() {
   // This could be difficult
   // This requires Google Map API, weather API
+
+  // https://i.pinimg.com/originals/65/e7/63/65e763df894b30b767e3134675d83767.jpg <-- inspiration
 
   return (
     <>
@@ -18,12 +21,29 @@ function LocationHomePage() {
               <div className={styles["article-title"]}>
                 Our Physical Location
               </div>
-              <div className={styles["article-subContainer"]}>
-                <div className={styles["article-subTitle"]}>Jubilee Market</div>
-                <div className={styles["article-content"]}>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-                  cumque libero, doloremque obcaecati vero esse! Ab harum
-                  repellat unde eveniet.
+              <div className={styles["article-card"]}>
+                <div className={styles["article-subContainer"]}>
+                  <div className={styles["article-top"]}>
+                    <div className={styles["article-subTitle"]}>
+                      Jubilee Market
+                    </div>
+                    <div className={styles["article-address"]}>
+                      1 Tavistock St, London WC2E 8BD
+                    </div>
+                  </div>
+                  <div className={styles["article-bottom"]}>
+                    <img
+                      src={img}
+                      alt="jewellery"
+                      className={styles["open-sign"]}
+                    ></img>
+                    <div className={styles["article-timeBox"]}>
+                      <div className={styles["article-days"]}>
+                        Saturday - Sunday
+                      </div>
+                      <div className={styles["article-times"]}>8am to 6pm!</div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
