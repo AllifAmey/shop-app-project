84a13544a425f695b10b4a4ca90b6e09d2d43ba0
AllifAmey <24247758+AllifAmey@users.noreply.github.com>
2022-11-24 18:15:05 +0000

    changed shop styles to added more readability to the shopPage code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import NavBar from "./components/NavBar";
+
 import ShopPage from "./components/pages/ShopPage";
 import ProductInfoPage from "./components/pages/ProductInfoPage";
 import HomePage from "./components/pages/HomePage";
 import Footer from "./components/Footer";
 import StoryPage from "./components/pages/StoryPage";
 import ProductImage from "./components/ProductImage";
-import ProductHomePage from "./components/ProductHomePage";
 import ContactPage from "./components/pages/ContactPage";
 import FAQPage from "./components/pages/FAQPage";
 import AccountPage from "./components/pages/AccountPage";
@@ -37,7 +37,7 @@ function App() {
             <Route path="/" element={<Navigate to="/home" />} />
             <Route path="/home" element={<HomePage />} />
             <Route path="/account/:accessType" element={<AccountPage />} />
-            <Route path="/test" element={<ProductImage />} />
+            <Route path="/test" element={<ShopPage />} />
             <Route path="/checkout" element={<CheckOutPage />} />
             <Route path="/shop" element={<ShopPage />} />
             <Route path="/product/:productId" element={<ProductInfoPage />} />
--- src/components/pages/ShopPage.js
+++ src/components/pages/ShopPage.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
-import styles from "./ShopPage.module.css";
 import ProductCard from "../ProductCard";
 import Checkbox from "@mui/material/Checkbox";
 import { useSelector } from "react-redux";
-import img from "../../img/Cards/Product/card-ring.avif";
+import imgPin from "../../img/icons/pinCushion-icon.png";
+import imgSrunchy from "../../img/icons/scrunchies-icon.png";
+import imgRing from "../../img/icons/ring-icon.png";
 import AnimatedPage from "../utility/AnimatedPage";
+import { Grid } from "@mui/material";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
-function ProductBox() {
+function ShopPage() {
   /*
 
   Make the shopping button light grey.
@@ -40,77 +42,121 @@ function ProductBox() {
     setPinCushionChecked(event.target.checked);
   };
 
+  const filterItemStyle = {
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    height: "20%",
+    width: "100%",
+  };
+
+  const filterNameStyle = {
+    width: "80%",
+    fontSize: "20px",
+  };
+
+  const filterImgStyle = {
+    height: "25px",
+    width: "25px",
+  };
+
   return (
     <>
       <AnimatedPage>
-        <div className={styles["container"]}>
-          <div className={styles["section-container"]}>
-            <div className={styles["left-container"]}>
-              <div className={styles["filter-container"]}>
-                <div className={styles["filter-title"]}>Items</div>
-                <div className={styles["filter-item"]}>
-                  <div className={styles["filter-name"]}>
-                    <Checkbox
-                      {...label}
-                      checked={pinCUshionChecked}
-                      onChange={handlePinCushionFilter}
-                    />
-                    Pin Cushions
-                  </div>
-                  <div className={styles["filter-icon-pin"]}></div>
-                </div>
-                <div className={styles["filter-item"]}>
-                  <div className={styles["filter-name"]}>
-                    <Checkbox
-                      {...label}
-                      checked={scrunchiesChecked}
-                      onChange={handleSrunchiesFilter}
-                    />
-                    Scrunchies
-                  </div>
-                  <div className={styles["filter-icon-scrunchies"]}></div>
-                </div>
-                <div className={styles["filter-item"]}>
-                  <div className={styles["filter-name"]}>
-                    <Checkbox
-                      {...label}
-                      checked={ringChecked}
-                      onChange={handleRingFilter}
-                    />
-                    Rings
-                  </div>
-                  <div className={styles["filter-icon-ring"]}></div>
-                </div>
-              </div>
-            </div>
-            <div className={styles["right-container"]}>
-              {Object.keys(shop)
-                .filter((cardName) => {
-                  console.log(shop[cardName].type.replace(/\s/g, ""));
-                  console.log(filters[shop[cardName].type.replace(/\s/g, "")]);
-                  if (filters[shop[cardName].type.replace(/\s/g, "")]) {
-                    return true;
-                  } else {
-                    return false;
-                  }
-                })
-                .map((cardName) => {
-                  return (
-                    <ProductCard
-                      key={cardName}
-                      type={shop[cardName].type.toLowerCase()}
-                      img={shop[cardName].img}
-                      price={`£  ${shop[cardName].price.toString()}`}
-                      cardName={cardName}
-                    ></ProductCard>
-                  );
-                })}
-            </div>
-          </div>
-        </div>
+        <Grid
+          container
+          height="auto"
+          width={1}
+          justifyContent="center"
+          margin="2rem 0 "
+        >
+          <Grid
+            item
+            container
+            alignSelf="start"
+            flexDirection="column"
+            justifyContent="start"
+            alignContent="center"
+            xs={2}
+          >
+            <Grid container flexDirection="column" height={0.5} width={0.9}>
+              <Grid fontSize={30} textAlign="center" padding="20px 0 40px">
+                Items
+              </Grid>
+              <Grid container sx={filterItemStyle}>
+                <Grid item sx={filterNameStyle}>
+                  <Checkbox
+                    {...label}
+                    checked={pinCUshionChecked}
+                    onChange={handlePinCushionFilter}
+                  />
+                  Pin Cushions
+                </Grid>
+                <Grid sx={filterImgStyle} component="img" src={imgPin}></Grid>
+              </Grid>
+              <Grid container sx={filterItemStyle}>
+                <Grid item sx={filterNameStyle}>
+                  <Checkbox
+                    {...label}
+                    checked={scrunchiesChecked}
+                    onChange={handleSrunchiesFilter}
+                  />
+                  Scrunchies
+                </Grid>
+                <Grid
+                  sx={filterImgStyle}
+                  component="img"
+                  src={imgSrunchy}
+                ></Grid>
+              </Grid>
+              <Grid container sx={filterItemStyle}>
+                <Grid item sx={filterNameStyle}>
+                  <Checkbox
+                    {...label}
+                    checked={ringChecked}
+                    onChange={handleRingFilter}
+                  />
+                  Rings
+                </Grid>
+                <Grid sx={filterImgStyle} component="img" src={imgRing}></Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            container
+            height="100vh"
+            width={0.8}
+            gap={2}
+            justifyContent="start"
+            alignItems="start"
+            xs={9}
+          >
+            {Object.keys(shop)
+              .filter((cardName) => {
+                console.log(shop[cardName].type.replace(/\s/g, ""));
+                console.log(filters[shop[cardName].type.replace(/\s/g, "")]);
+                if (filters[shop[cardName].type.replace(/\s/g, "")]) {
+                  return true;
+                } else {
+                  return false;
+                }
+              })
+              .map((cardName) => {
+                return (
+                  <ProductCard
+                    key={cardName}
+                    type={shop[cardName].type.toLowerCase()}
+                    img={shop[cardName].img}
+                    price={`£  ${shop[cardName].price.toString()}`}
+                    cardName={cardName}
+                  ></ProductCard>
+                );
+              })}
+          </Grid>
+        </Grid>
       </AnimatedPage>
     </>
   );
 }
 
-export default ProductBox;
+export default ShopPage;
