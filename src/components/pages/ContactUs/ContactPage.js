cd612fd7a3bd037dedb46564e44d4ed6ad5a08ca
AllifAmey <24247758+AllifAmey@users.noreply.github.com>
2023-01-22 17:09:41 +0000

    adjusted the contactpage to fit original design intention and added documentation for future understanding

--- src/components/pages/ContactUs/ContactPage.js
+++ src/components/pages/ContactUs/ContactPage.js
@@ -3,15 +3,33 @@ import styles from "./ContactPage.module.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import AnimatedPage from "../../utility/AnimatedPage";
-import { useParams } from "react-router-dom";
 import { MuiTelInput } from "mui-tel-input";
 import emailjs from "@emailjs/browser";
+import PhoneIcon from "@mui/icons-material/Phone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
 function ContactPage() {
   /**
    *https://i.ytimg.com/vi/ZafzM_z9PLs/maxresdefault.jpg
    Contact page with picture under the phone number.
    https://viclafouch.github.io/mui-tel-input/docs/getting-started/ phone number. 
+   */
+
+  /*
+   docs - 
+    Logic for layout-
+      The way it works is simple. It's flexboxes on top of flexboxes to give the layout from the link above.
+      The idea is to have a main flexbox and then to have a flexbox on top of that using absolute to move on top,
+      and shift to the left to give this sort of feel that it is on top.
+      The main reason for this design is due to intuition. I feel it looks good .
+    
+    future - 
+
+    Perhaps the look of the layout needs to change but not much else.
+    For scaling, perhaps turn the current backend into something that can handle emails which is possible.
+    The emailjs usage is quite minimal but that is anticipating audience. 
+
+
    */
 
   const [phone, setPhone] = React.useState("");
@@ -56,23 +74,19 @@ function ContactPage() {
                       Contact Info
                     </div>
                     <div className={styles["contactInfo-item"]}>
-                      <img
-                        src="https://img.icons8.com/fluency/28/null/circled-envelope.png"
-                        alt="email"
-                        className={styles["contactInfo-img"]}
+                      <MailOutlineIcon
+                        sx={{ height: "40px", width: "40px", color: "#112A46" }}
                       />
                       <div className={styles["contactInfo-text"]}>
-                        <div>lorem@ipsum.com</div>
+                        <div>Ameyssssd@aol.com</div>
                       </div>
                     </div>
                     <div className={styles["contactInfo-item"]}>
-                      <img
-                        src="https://img.icons8.com/external-anggara-blue-anggara-putra/24/null/external-telephone-interface-anggara-blue-anggara-putra.png"
-                        alt="Company phone number"
-                        className={styles["contactInfo-img"]}
+                      <PhoneIcon
+                        sx={{ height: "40px", width: "40px", color: "#112A46" }}
                       />
                       <div className={styles["contactInfo-text"]}>
-                        <div>310-386-1623</div>
+                        <div>07 305 588 089</div>
                       </div>
                     </div>
                   </div>
