0d0deb0342cb096e11feced7ace71d6e66219213
AllifAmey <24247758+AllifAmey@users.noreply.github.com>
2023-04-25 23:22:50 +0100

    made a design decision to get rid of googlemap api as content is static and predictable, this reduces initial impact,improves performance and reduces reliance on third parties unnecessarily

---
+++ dist/assets/useFormControl-e1de9fb1.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-e6a35aa4.js";const t=o.createContext(void 0),r=t;function e(){return o.useContext(r)}export{r as F,e as u};
--- src/components/pages/Home/LocationHomePage.jsx
+++ src/components/pages/Home/LocationHomePage.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect, useCallback, lazy, Suspense } from "react";
+import { useState, useEffect, useCallback } from "react";
 import styles from "./LocationHomePage.module.css";
 import img from "../../assets/img/icons/openSign-icon.png";
 import CircularProgress from "@mui/material/CircularProgress";
-import OurLocation from "./OurLocation";
 import { useInView } from "react-intersection-observer";
+import googlemap from "../../assets/img/Homepage/our-location-google-map.avif";
 
 function LocationHomePage() {
   // https://i.pinimg.com/originals/65/e7/63/65e763df894b30b767e3134675d83767.jpg <-- inspiration
@@ -24,6 +24,13 @@ function LocationHomePage() {
   There may be a case to split the components or change the layout for more ,
   market locations , in the event the business expands to different locations.
 
+  current - 
+
+  The decision has been to not rely on google map api as it is far too slow and ,
+  not really necessary. Upon the expansion of the business, I will include google map,
+  api again. 
+  Instead a snapshot has been taken and a img tag is used to display it.
+  Weather api remains unchanged unlike the google map api it is not static and needs update.
 
   */
 
@@ -61,7 +68,12 @@ function LocationHomePage() {
       <div className={styles["main-container"]} id="location" ref={ref}>
         {inView ? (
           <div className={styles["section-container"]}>
-            <OurLocation> </OurLocation>
+            <img
+              src={googlemap}
+              alt="google map"
+              loading="lazy"
+              style={{ height: "100%", width: "50%", objectFit: "cover" }}
+            />
             <div className={styles["right-container"]}>
               <div className={styles["article-container"]}>
                 <div className={styles["article-heading"]}>
